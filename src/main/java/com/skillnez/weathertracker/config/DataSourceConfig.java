ba7a3ed79e52a386b8af8ec6f611495649471c1b
Skillnez <skillnezwork@gmail.com>
2025-05-17 14:41:20 +0300

    fix: fix DataSourceConfig and FlywayConfig

--- src/main/java/com/skillnez/weathertracker/config/DataSourceConfig.java
+++ src/main/java/com/skillnez/weathertracker/config/DataSourceConfig.java
@@ -1,21 +1,33 @@
 package com.skillnez.weathertracker.config;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 public class DataSourceConfig {
+
+    @Value("${db.driver}")
+    private String driver;
+    @Value("${db.url}")
+    private String url;
+    @Value("${db.user}")
+    private String user;
+    @Value("${db.password}")
+    private String password;
 
     @Bean
     public DataSource dataSource() {
         BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/weather_tracker_db");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("root"); //TODO не забудь поменять при деплое
+        dataSource.setDriverClassName(driver);
+        dataSource.setUrl(url);
+        dataSource.setUsername(user);
+        dataSource.setPassword(password);
         return dataSource;
     }
 }
